7733cd42ede8b2609d3271289eebb719ab6bd788
adthor <adam@thalchemy.com>
2017-04-09 00:07:59 -0500

    mysql

--- adamthorson/settings/testing.py
+++ adamthorson/settings/testing.py
@@ -4,3 +4,14 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': get_environment_variable('DATABASE_NAME'),
+        'USER': get_environment_variable('DATABASE_USER'),
+        'PASSWORD': get_environment_variable('DATABASE_PASSWORD'),
+        'HOST': '',
+        'PORT': '',
+    }
+}
