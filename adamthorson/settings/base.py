19844ec2c9a9900cedddd0c6aa97681e3ef73546
adthor <adam@thalchemy.com>
2017-04-10 22:07:14 -0500

    included blog and core

--- adamthorson/settings/base.py
+++ adamthorson/settings/base.py
@@ -50,6 +50,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Custom apps
+    'adamthorson.apps.blog',
+    'adamthorson.apps.core',
 )
 
 MIDDLEWARE_CLASSES = (
