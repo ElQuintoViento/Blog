bb8d24c9213417f5468e41676ccfef7b3fffdff8
adthor <adam@thalchemy.com>
2017-04-12 15:11:51 -0500

    simple url redirect of posts, search posts, and search pg

--- adamthorson/apps/blog/urls.py
+++ adamthorson/apps/blog/urls.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
+# from django.views.generic.base import RedirectView
 #
 from adamthorson.apps.blog.views import *
 
 
 urlpatterns = [
-    url(r'^view/(?P<slug>[^\.]+)$', view_post),
+    # Search posts
+    url(r'^$', view_posts),
+    url(r'^search/$', search_posts),
+    # View posts
+    url(r'^(?P<slug>[^\.]+).html$', ViewPostRedirectView.as_view()),
+    url(r'^(?P<slug>[^\.]+)$', view_post),
 ]
