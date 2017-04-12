984c2a48c8d6755578e42546e2ef16a31f704bdb
adthor <adam@thalchemy.com>
2017-04-12 15:13:16 -0500

    ViewPostRedirectView, simple search_posts

--- adamthorson/apps/blog/views.py
+++ adamthorson/apps/blog/views.py
@@ -1,7 +1,27 @@
 # -*- coding: utf-8 -*-
+import re
+#
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, render
+from django.views.generic.base import RedirectView
 #
 from adamthorson.apps.blog.models import Post
+
+
+class ViewPostRedirectView(RedirectView):
+    permanent = False
+    query_string = True
+
+    def get_redirect_url(self, slug):
+        return slug
+
+
+def search_posts(request):
+    return JsonResponse("[{title: 'junk'}]", safe=False)
+
+
+def view_posts(request):
+    return render(request, 'view_posts.html', {})
 
 
 def view_post(request, slug):
