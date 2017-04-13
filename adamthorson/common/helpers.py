22f8c6ed03f220dd3ce44a999e9f832b8386e914
adthor <adam@thalchemy.com>
2017-04-13 17:10:40 -0500

    basic json response

--- adamthorson/common/helpers.py
+++ adamthorson/common/helpers.py
@@ -1,1 +1,17 @@
 # -*- coding: utf-8 -*-
+from django.http import HttpResponse
+#
+from rest_framework.renderers import JSONRenderer
+
+
+# class JSONResponse(HttpResponse):
+#     def __init__(self, json, **kwargs):
+#         super(JSONResponse, self).__init__(
+#             json, content_type="application/json", **kwargs)
+
+# An HttpResponse that renders its content into JSON.
+class JSONResponse(HttpResponse):
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
