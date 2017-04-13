786be0dcd29376d192b386cbd550a1e87d4be5d2
adthor <adam@thalchemy.com>
2017-04-13 17:09:05 -0500

    switching to CBVs

--- adamthorson/apps/blog/views.py
+++ adamthorson/apps/blog/views.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 import re
 #
-from django.http import JsonResponse
+from django.core import serializers
 from django.shortcuts import get_object_or_404, render
+from django.views.generic import View
 from django.views.generic.base import RedirectView
 #
 from adamthorson.apps.blog.models import Post
+from adamthorson.apps.blog.serializers import *
+from adamthorson.common.helpers import JSONResponse
 
 
 class ViewPostRedirectView(RedirectView):
@@ -16,14 +19,35 @@ class ViewPostRedirectView(RedirectView):
         return slug
 
 
-def search_posts(request):
-    return JsonResponse("[{title: 'junk'}]", safe=False)
+# def search_posts(request):
+#     # json = Post.objects.values()
+#     # return HttpResponse(json, content_type="application/json")
+#     JSONSerializer = serializers.get_serializer("json")
+#     serializer = JSONSerializer()
+#     return serializer.serialize(Post.objects.all())
+class SearchPostsView(View):
+    def get(self, request, *args, **kwargs):
+        data = Post.objects.all()
+        serialized_data = PostSerializer(data, many=True).data
+        return JSONResponse(serialized_data)
 
 
-def view_posts(request):
-    return render(request, 'view_posts.html', {})
+# def view_posts(request):
+#    return render(request, 'view_posts.html', {})
+class ViewPostsView(View):
+    template_name = 'view_posts.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(
+            request, self.template_name, {})
 
 
-def view_post(request, slug):
-    post = get_object_or_404(Post, slug=slug)
-    return render(request, 'view_post.html', {'post': post})
+# def view_post(request, slug):
+#     post = get_object_or_404(Post, slug=slug)
+#     return render(request, 'view_post.html', {'post': post})
+class ViewPostView(View):
+    template_name = 'view_post.html'
+
+    def get(self, request, slug):
+        post = get_object_or_404(Post, slug=slug)
+        return render(request, 'view_post.html', {'post': post})
