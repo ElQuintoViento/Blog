302c69f7c55277201f8e1698197abf2c8feb2b02
adthor <adam@thalchemy.com>
2017-04-14 14:51:04 -0500

    how can PostSerializer inherit SimplePostSerializer?

--- adamthorson/apps/blog/serializers.py
+++ adamthorson/apps/blog/serializers.py
@@ -1,12 +1,30 @@
 # -*- coding: utf-8 -*-
 from adamthorson.apps.blog.models import *
+from adamthorson.apps.core.serializers import *
 from adamthorson.common.serializers import *
 
 
-class PostSerializer(BaseSerializable):
-    title = serializers.CharField()
+class SeriesSerializer(TitleSerializable):
+    class Meta:
+        model = Series
+        fields = ['title']
+
+
+class SimplePostSerializer(PublishSerializable, SimpleTagSerializable,
+                           TimestampSerializable, TitleSerializable):
+    series = serializers.CharField(source='series.title')
 
     class Meta:
         model = Post
-        fields = ['title']
-        depth = 1
+        fields = ['created_date', 'modified_date', 'publish_date',
+                  'series', 'title', 'tags']
+        depth = 2
+
+
+class PostSerializer(PublishSerializable, SimpleTagSerializable,
+                     TimestampSerializable, TitleSerializable):
+    class Meta:
+        model = Post
+        fields = ['content', 'created_date', 'modified_date', 'publish_date',
+                  'series', 'title', 'tags']
+        depth = 2
