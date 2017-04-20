75e14f0ec29046d50f7500023aed3f14d4269b10
adthor <adam@thalchemy.com>
2017-04-20 18:54:34 -0500

    accesses csrf token for posts; separated out table population logic into other functions

--- adamthorson/static/js/view_posts.js
+++ adamthorson/static/js/view_posts.js
@@ -1,28 +1,62 @@
 $(document).ready(function(){
+    var csrftoken = $('input[name=csrfmiddlewaretoken]').val();
+
+    var blog_posts = [];
+
+
     var clearTable = function(table){
         $('#' + table + ' tr').remove();
     }
 
 
+    var clearDataAndTable = function(data_array, table){
+        for(var i=data_array.length; i > 0; --i){
+            data_array.pop();
+        }
+        clearTable(table);
+    }
+
+
     var createTableRow = function(post){
         // Post attributes
+        var slug = post['slug'];
         var title = post['title'];
-        var img_url = "/static/img/blogging.jpg"
+        var img_url = "/static/img/blogging.jpg";
+        var post_url = "/blog/" + slug;
         // UI setup
-        var img_div = $('<div/>', {
-            'class': 'img',
-            'css': 'background-image: url(\'' + img_url + '\')',
-        });
+        // Left div
+        // Post image & link
+        var img_div = $('<div/>', {'class': 'img'});
+        img_div.css('background-image', "url('" + img_url + "')");
+        
+        var a_img = $('<a/>', {'href': post_url});
+        a_img.append(img_div);
+
+        var left_div = $('<div/>', {'class': 'left'});
+        left_div.append(a_img);
+
+        // Right div
+        // Attributes
         var title_div = $('<div/>', {
             'class': 'title',
             'text': title
         });
-        var left_div = $('<div/>', {'class': 'left'});
+        var tags_div = $('<div/>', {'class': 'tags', 'text': 'tags'});
+        var date_p = $('<p/>', {
+            'text': formatDate(post['created_date'])
+        });
+        var bottom_div = $('<div/>', {'class': 'bottom'});
+        bottom_div.append(date_p);
+
+        var a_title = $('<a/>', {'href': post_url});
+        a_title.append(title_div);
+    
         var right_div = $('<div/>', {'class': 'right'});
+        right_div.append(a_title);
+        right_div.append(tags_div);
+        right_div.append(bottom_div);
 
-        left_div.append(img_div);
-        right_div.append(title_div);
-
+        // Combine left and right divs
         var post_div = $('<div/>', {'class': 'td_div_search_post'});
 
         post_div.append(left_div);
@@ -35,32 +69,68 @@ $(document).ready(function(){
     }
 
 
-    var loadTable = function(table, result){
-        clearTable(table);
-
-        /*$.each(result, function(){
-            $.each(this, function(k, v){
-                str += k + " " + v + "\n";
-            });
-        });*/
+    var storeTableData = function(data_array, result){
         for(var i=0; i < result.length; ++i){
             var post = result[i];
-            for(var j=0; j < 4; ++j){
-                var row = createTableRow(post);
-                $('#' + table + ' tbody').append(row);
-            }
+            var row = createTableRow(post);
+            
+            data_array.push({
+                'title': post['title'],
+                'html': row
+            });
         }
     }
 
 
+    var pushDataToTable = function(data_array, table){
+        for(var i=0; i < data_array.length; ++i){
+            $('#' + table + ' tbody').append(
+                data_array[i]['html']);
+        }
+    }
+
+
+    var loadTable = function(data_array, table){
+        $.ajax({
+            beforeSend: function (xhr, settings) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            },
+            method: 'POST',
+            url: 'search/',
+            success: function(result){
+                console.log(JSON.stringify(result));
+                clearDataAndTable(data_array, table);
+                storeTableData(data_array, result);
+                pushDataToTable(data_array, table);
+            }
+        });
+    }
+
+
+    /*var sort = function(data_array, table){
+        for(var i=0; i < (data_array.length - 1); ++i){
+            var row_a = data_array[i];
+            var row_b = data_array[i + 1];
+
+            if(row_a['title'].toLowerCase() < row_b['title'].toLowerCase()){
+                data_array[i] = row_b;
+                data_array[i + 1] = row_a;
+            }
+        }
+
+        clearTable(table);
+        pushDataToTable(data_array, table);
+    }*/
+
+
     $('#search_input').keydown(function(event){
-        if(event.which === 13){
-            $.ajax({
-                url: 'search/',
-                success: function(result){
-                    loadTable('search_table', result);
-                }
-            });
+        switch(event.which){
+            case 13:
+                loadTable(blog_posts, 'search_table')
+                break;
+            /*case 90:
+                sort(blog_posts, 'search_table');
+                break;*/
         }
     });
 });
