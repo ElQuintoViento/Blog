11a73f7fa4e89fdfdf63069e406b290e32cf8627
adthor <adam@thalchemy.com>
2017-04-21 14:57:02 -0500

    error message; alphanumeric regex checker

--- adamthorson/static/js/helpers.js
+++ adamthorson/static/js/helpers.js
@@ -1,3 +1,35 @@
+var showError = function(message, tag, ms){
+    var $body = $(document.body);
+    var message_p = $('<p/>', {'text': message});
+    var error_div = $('<div/>', {
+        'class': ('error_window float_center ' + tag)
+    });
+
+    error_div.append(message_p);
+
+    $(document.body).append(error_div);
+
+    var removeError = function(){
+        error_div.remove();
+    };
+
+    setTimeout(function(){ 
+        error_div.fadeOut("slow", removeError);
+    }, ms );
+};
+
+
+var isErrorVisible = function(tag){
+    return ($('.error_window, .' + tag).length < 1);
+}
+
+
+var checkAlphanumeric = function(str){
+    var pattern = /[^a-zA-Z0-9\s]/g;
+    return (!pattern.test(str));
+};
+
+
 var formatDate = function(epoch){
     var utcDate = new Date(epoch);
     var year = utcDate.getFullYear();
@@ -5,4 +37,4 @@ var formatDate = function(epoch){
     var day = utcDate.getDate();
 
     return (month + "/" + day + "/" + year);
-}
+};
