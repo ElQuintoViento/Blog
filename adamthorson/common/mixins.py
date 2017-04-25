414400739f10d5cd33d34628c8186974ccdfcdb2
adthor <adam@thalchemy.com>
2017-04-25 16:29:52 -0500

    SearchMixin filters using Tags as well (only implemented for Post right now

--- adamthorson/common/mixins.py
+++ adamthorson/common/mixins.py
@@ -1,12 +1,24 @@
 # -*- coding: utf-8 -*-
+import re
+#
 from django.apps import apps
+from django.db.models import Q
 from django.db.models.query import QuerySet
 
 
 class SearchMixin(QuerySet):
     def search_filter(self, text):
-        # if self.model is apps.get_model('blog', 'Post'):
-        return self.filter(title__icontains=text)
+        queryset = self
+        queryset = queryset.filter(title__icontains=text)
+        #
+        if self.model is apps.get_model('blog', 'Post'):
+            separated_text = re.findall(r'[a-zA-Z0-9]+', text)
+            q = Q()
+            for word in separated_text:
+                q |= Q(tags__name__iexact=word)
+            querset_q = self.filter(q)
+            queryset |= querset_q
+        return queryset.distinct().order_by('-publish_date')
 
     class Meta:
         abstract = True
