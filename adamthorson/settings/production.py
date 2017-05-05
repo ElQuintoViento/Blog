18c83634e05f9bc934fd308716320bf70a3caa1a
adthor <adam@thalchemy.com>
2017-05-05 14:20:27 -0500

    added allowed hosts

--- adamthorson/settings/production.py
+++ adamthorson/settings/production.py
@@ -5,5 +5,10 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+]
+
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATICFILES_DIRS = ()
