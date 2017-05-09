9c0dac91ef3af20ebf4257e8e81890a71e0dbb0c
adthor <adam@thalchemy.com>
2017-05-09 14:41:58 -0500

    moved logging config to dev and prod settings

--- adamthorson/settings/development.py
+++ adamthorson/settings/development.py
@@ -15,3 +15,45 @@ DATABASES = {
         'PORT': '',
     }
 }
+
+# Logging configurations
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(LOG_DIR, "blog.log"),
+            'formatter': 'verbose'
+        },
+        'dbfile': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(LOG_DIR, "db.log"),
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+        'django.db.backends': {
+            'handlers': ['dbfile'],
+            'propagate': False,
+            'level': 'DEBUG',
+        },
+        'django.request': {
+            'handlers': ['file'],
+            'propagate': False,
+            'level': 'DEBUG',
+        }
+    }
+}
