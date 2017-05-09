bb282d40c49af945d71de2c17d7afed22919161f
adthor <adam@thalchemy.com>
2017-05-09 13:09:01 -0500

    comment update

--- adamthorson/settings/base.py
+++ adamthorson/settings/base.py
@@ -138,7 +138,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-# Python configurations
+# Logging configurations
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
