54fd10d52e71938f3766e31e7c3d7c4e2e912f12
adthor <adam@thalchemy.com>
2017-05-09 14:42:16 -0500

    moved logging config to dev and prod settings

--- adamthorson/settings/production.py
+++ adamthorson/settings/production.py
@@ -27,3 +27,45 @@ DATABASES = {
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATICFILES_DIRS = ()
+
+# Logging configurations
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(LOG_DIR, "blog.log"),
+            'formatter': 'verbose'
+        },
+        'dbfile': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(LOG_DIR, "db.log"),
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+        'django.db.backends': {
+            'handlers': ['dbfile'],
+            'propagate': False,
+            'level': 'DEBUG',
+        },
+        'django.request': {
+            'handlers': ['file'],
+            'propagate': False,
+            'level': 'DEBUG',
+        }
+    }
+}
